a9ed074d013adc5b0808577fc26dfb0c5e32a55b
Samil Gencaslan <samil-gencaslan@outlook.com>
2020-09-28 10:58:58 +0200

    init

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,63 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  name: string = '';
+  namelist: any = this.getNames();
 
-  constructor() {}
+  constructor(public storage: Storage) {
+    
+  }
+
+  add() {
+    if (this.name.length > 0) {
+      let task = this.name;
+      this.namelist.push(task);
+      this.storage.set('namelist', JSON.stringify(this.namelist));
+      this.name = '';
+    }
+  }
+
+  delete(index) {
+    this.namelist.splice(index, 1);
+    this.storage.set('namelist', JSON.stringify(this.namelist));
+    
+  }
+
+  getNames(): Promise<any> {
+    let namelist = this.storage.get('namelist');
+    return namelist;
+  }
 
 }
+  /*add() {
+    if (this.name.length > 0) {
+      let name = this.name;
+      this.namelist.push(name);
+      this.storage.set('namelist', JSON.stringify(this.namelist));
+      this.name = '';
+    }
+  }
+
+  add(name:any):void {
+    if (this.name.length > 0) {
+      let name = this.name;
+      this.storage.set('username',name);
+      this.name = '';
+    }
+    let namelist = this.storage.get('namelist');
+    if (namelist) {
+      namelist.push(name);
+      this.storage.set('namelist', namelist);
+    } else {
+      namelist = [];
+      namelist.push(name);
+      this.storage.set('favorites', namelist);
+    }}
+
+  delete(index) {
+    this.namelist.splice(index, 1);
+    this.storage.set('namelist', JSON.stringify(this.namelist));
+  }
+
+   */
+
